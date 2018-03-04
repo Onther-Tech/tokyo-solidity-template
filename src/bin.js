24eb0a895937a4889c41120f46f7ea4416616ef6
Ju-hyeoung, Park <dkdkajej@gmail.com>
2018-03-04 17:41:12 +0900

    bin script added

---
+++ src/bin.js
@@ -0,0 +1,27 @@
+require("babel-register");
+require("babel-polyfill");
+
+import program from "commander";
+
+import template, { defaultInputPath, defaultOutputPath } from "./index";
+
+program
+  .option("-i, --input [json path]", `input json path. default ${ defaultInputPath }`)
+  .option("-o, --output [output dir path]", `output directory path. default ${ defaultOutputPath }`)
+  .parse(process.argv);
+
+async function main() {
+  const {
+    input = defaultInputPath,
+    output = defaultOutputPath,
+  } = program;
+
+  const options = { input, output };
+
+  return template(options, () => {
+    console.log(`truffle project is generated at ${ output }`);
+  });
+}
+
+main()
+  .catch(console.error);
